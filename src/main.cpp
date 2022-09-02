245f03798f7a65d9f905fcb7a5a872117c0c3418
xue12311 <1712530035@qq.com>
2022-09-02 23:59:38 +0800

    变更

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,163 @@
 #include <Arduino.h>
-#include <WiFiManager.h>
-#include <MQTTManager.h>
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+#include "FS.h"
 
-/**
- * wifi 连接
- */
-WiFiManager wifi_manager;
-/**
- * mqtt 连接
- */
-MQTTManager mqtt_manager;
+#define LED 2
+
+
+void ConnectServer();
+
+bool WiFiWatchDog();
+
+void BaseConfig();
+
+void SmartConfig();
+
+
+struct Config {
+    String SSID;
+    String Passwd;
+    String Server = "esaylinker";
+    String Token = "0000";
+};
+Config _config;
+
+bool LoadConfig() {
+    File configFile = SPIFFS.open("/config.json", "r");
+    if (!configFile) {
+        Serial.println("Failed to open config file");
+        return false;
+    }
+    StaticJsonDocument<1024> doc;
+    DeserializationError error = deserializeJson(doc, configFile);
+    if (error) {
+        Serial.println("Failed to read file, using default configuration");
+        return false;
+    }
+    _config.SSID = doc["SSID"] | "fail";
+    _config.Passwd = doc["Passwd"] | "fail";
+    if (_config.SSID == "fail" || _config.Passwd == "fail") {
+        return false;
+    } else {
+        Serial.println("Load wifi config from spiffs successful.");
+        Serial.print("Loaded ssid: ");
+        Serial.println(_config.SSID);
+        Serial.print("Loaded passwd: ");
+        Serial.println(_config.Passwd);
+        return true;
+    }
+}
+
+bool SaveConfig() {
+    DynamicJsonDocument doc(1024);
+    JsonObject root = doc.to<JsonObject>();
+    root["SSID"] = _config.SSID;
+    root["Passwd"] = _config.Passwd;
+    root["Server"] = _config.Server;
+    root["Token"] = _config.Token;
+
+    File configFile = SPIFFS.open("/config.json", "w");
+    if (!configFile) {
+        Serial.println("Failed to open config file for writing");
+        return false;
+    }
+    if (serializeJson(doc, configFile) == 0) {
+        Serial.println("Failed to write to file");
+        return false;
+    }
+    return true;
+}
 
 void setup() {
     Serial.begin(9600);
-    //初始化 mqtt 服务器
-    mqtt_manager.initMqttServer();
-    //通过 已保存本地的wifi配置 进行wifi连接
-    if (wifi_manager.onConnectWiFiConfigJson()) {
-        Serial.println("缓存 连接 wifi 成功.");
-        //连接 mqtt 成功
-        if(wifi_manager.onConnectMqttService()){
-            //订阅 mqtt 主题
-            wifi_manager.onSubscribeMqttTopic();
-        }
-    } else {
-        Serial.println("缓存 连接 wifi 失败.");
-        if (wifi_manager.onStartWiFiAPAndWebServer()) {
-            Serial.println("启用网络服务成功.");
-        } else {
-            Serial.println("启用网络服务失败.");
-        }
+    //Set LED
+    pinMode(LED, OUTPUT);
+    //Mount FS
+    Serial.println("Mounting FS...");
+    if (!SPIFFS.begin()) {
+        Serial.println("Failed to mount file system");
+        return;
     }
+    //Connect WIFI
+    ConnectServer();
 }
 
 void loop() {
-    //wifi 连接成功
-    if (wifi_manager.isWiFiConnected()) {
-        mqtt_manager.onMQTTServerLoop();
+    //Wifi watch dog,if wifi not connect, it will auto reconnect.
+    WiFiWatchDog();
+
+    Serial.print("Looks like working. now:");
+    Serial.println(millis() / 1000);
+    delay(1000);
+}
+
+void ConnectServer() {
+    if (LoadConfig()) {
+        BaseConfig();
     } else {
-        wifi_manager.onWebServerLoop();
+        SmartConfig();
     }
+    digitalWrite(LED, LOW);
 }
 
-/**
-  * 连接 mqtt 服务
-  * @return true 连接成功 false 连接失败
-  */
-bool WiFiManager::onConnectMqttService() {
-    return mqtt_manager.onConnectMqttServer();
+bool WiFiWatchDog() {
+    if (WiFi.status() != WL_CONNECTED) {
+        BaseConfig();
+    }
+    return true;
 }
 
-/**
- * 订阅 mqtt主题
- */
-void WiFiManager::onSubscribeMqttTopic() {
-    mqtt_manager.onSubscribeMqttTopic();
+void BaseConfig() {
+    Serial.println("Use base config to connect wifi.");
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(_config.SSID, _config.Passwd);
+    //连接超时时间，30s后没有连接将会转入SmartConfig
+    int timeout = 30000;
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.println("Wait to connect wifi...");
+        digitalWrite(LED, LOW);
+        delay(150);
+        digitalWrite(LED, HIGH);
+        delay(150);
+        timeout = timeout - 300;
+        if (timeout <= 0) {
+            Serial.println("Wifi connect timeout, use smart config to connect...");
+            SmartConfig();
+            return;
+        }
+    }
+    Serial.println("WiFi connected by base config.");
+    Serial.print("SSID:");
+    Serial.println(WiFi.SSID());
+    Serial.print("IP Address:");
+    Serial.println(WiFi.localIP());
+}
+
+void SmartConfig() {
+    Serial.println("Use smart config to connect wifi.");
+    WiFi.mode(WIFI_STA);
+    WiFi.beginSmartConfig();
+    while (1) {
+        Serial.println("Wait to connect wifi...");
+        digitalWrite(LED, LOW);
+        delay(1000);
+        digitalWrite(LED, HIGH);
+        delay(1000);
+        if (WiFi.smartConfigDone()) {
+            Serial.println("WiFi connected by smart config.");
+            Serial.print("SSID:");
+            Serial.println(WiFi.SSID());
+            Serial.print("IP Address:");
+            Serial.println(WiFi.localIP());
+            _config.SSID = WiFi.SSID();
+            _config.Passwd = WiFi.psk();
+            if (!SaveConfig()) {
+                Serial.println("Failed to save config");
+            } else {
+                Serial.println("Config saved");
+            }
+            break;
+        }
+    }
 }
